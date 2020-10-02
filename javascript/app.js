6d3a813c34dd8747251a24ec3d1c78d509c7a758
Keve1227 <killenlillen02@gmail.com>
2020-10-02 11:08:12 +0200

    Preview rendering

--- javascript/app.js
+++ javascript/app.js
@@ -44,16 +44,44 @@ app.viewport3D.init = function () {
     this.canvas = document.getElementById("preview-canvas");
     this.scene = new THREE.Scene();
     this.camera = new THREE.OrthographicCamera(0, 0, 0, 0, 0);
+
     this.renderer = new THREE.WebGLRenderer({
         canvas: this.canvas,
+        antialias: true,
+        alpha: true,
     });
+    this.renderer.setClearColor(0, 0);
+
+    let geometry = new THREE.BoxGeometry(25, 25, 25);
+    let material = new THREE.MeshNormalMaterial();
+    for (let i = 0; i < 300; i++) {
+        let cube = new THREE.Mesh(geometry, material);
+        cube.rotateX(Math.random() * 180);
+        cube.rotateY(Math.random() * 180);
+        cube.rotateZ(Math.random() * 180);
+        cube.position.x = Math.random() - 0.5;
+        cube.position.y = Math.random() - 0.5;
+        cube.position.z = Math.random() - 0.5;
+        cube.position.normalize();
+        cube.position.multiplyScalar(Math.random() * 200);
+        this.scene.add(cube);
+    }
 
     this.scene.add(this.camera);
-    this.setView({
-        width: this.canvas.clientWidth,
-        height: this.canvas.clientHeight,
-    });
+
+    update();
 };
+
+function update() {
+    requestAnimationFrame(() => update());
+
+    app.viewport3D.setView({
+        width: app.viewport3D.canvas.parentElement.clientWidth,
+        height: app.viewport3D.canvas.parentElement.clientHeight,
+    });
+
+    app.viewport3D.render();
+}
 
 app.viewport3D.invalidate = function () {
     this.render();
@@ -78,15 +106,16 @@ app.viewport3D.setView = function (view) {
     this.camera.left = -this.camera.right;
     this.camera.bottom = -this.camera.top;
 
+    // Apply position offsets
     this.camera.left += xOffset;
     this.camera.right += xOffset;
     this.camera.top += yOffset;
     this.camera.bottom += yOffset;
 
-    this.camera.updateProjectionMatrix();
-    this.renderer.setViewport(0, 0, width, height);
+    this.camera.position.z = 1000;
 
-    this.invalidate();
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(width, height);
 };
 
 module.exports = app;
