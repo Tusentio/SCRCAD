88a7e698b354f7ca1cffc77dc1a1a52056d4996d
Octaphous <pappkasse2121@gmail.com>
2020-10-06 13:53:41 +0200

    Grid rendering

--- javascript/viewport-2d.js
+++ javascript/viewport-2d.js
@@ -1,9 +1,12 @@
 module.exports = (app) => ({
     canvas: null,
+    /**
+     * @type {CanvasRenderingContext2D}
+     */
     context: null,
     modelPlane: null,
     view: {
-        zoom: 1,
+        zoom: 100,
         plane: "top",
     },
     init() {
@@ -14,14 +17,29 @@ module.exports = (app) => ({
     },
     invalidate() {
         this.setView({});
+        this.render();
     },
-    render() {},
+    render() {
+        let gridWidth = this.modelPlane.width + 2;
+        let gridHeight = this.modelPlane.height + 2;
+
+        this.context.lineWidth = 1;
+        this.context.strokeStyle = "rgba(80, 80, 80, 1)";
+
+        for (let x = 0; x < gridWidth; x++) {
+            for (let y = 0; y < gridHeight; y++) {
+                this.context.strokeRect(x * this.view.zoom, y * this.view.zoom, this.view.zoom, this.view.zoom);
+            }
+        }
+    },
     setView(view) {
         Object.assign(this.view, view);
         let { zoom, plane } = this.view;
 
         this.modelPlane = app.model.getPlane(plane);
-        this.canvas.width = this.modelPlane.width * zoom;
-        this.canvas.height = this.modelPlane.height * zoom;
+        this.canvas.width = (this.modelPlane.width + 2) * zoom;
+        this.canvas.height = (this.modelPlane.height + 2) * zoom;
+        
+        this.context = this.canvas.getContext("2d");
     },
 });
