51dc252f17a3cfd2124fba4ac7fa1c6e37fee25b
pappkasse <pappkasse2121@gmail.com>
2020-10-12 14:59:52 +0200

    Selection function

--- javascript/viewport-2d.js
+++ javascript/viewport-2d.js
@@ -18,6 +18,21 @@ module.exports = (app) => ({
             this.zoom(dy < 0 ? 1 : -1);
         });
 
+        this.canvas.addEventListener("click", (e) => {
+            let posX = e.clientX - e.target.offsetLeft;
+            let posY = e.clientY - e.target.offsetTop;
+
+            let tileX = Math.floor(posX / this.view.zoom) - 1;
+            let tileY = Math.floor(posY / this.view.zoom) - 1;
+
+            this.modelPlane.setVoxelAt(tileX, tileY, 0, {
+                selected: true,
+                color: 0xffffffff,
+            });
+
+            this.invalidate();
+        });
+
         this.setView({
             zoom: 100,
             plane: "top",
@@ -33,15 +48,60 @@ module.exports = (app) => ({
             });
     },
     render() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         let gridWidth = this.modelPlane.width + 2;
         let gridHeight = this.modelPlane.height + 2;
+
+        this.context.lineWidth = 6;
+
+        let gradient = this.context.createLinearGradient(
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height
+        );
+        gradient.addColorStop("0", "blue");
+        gradient.addColorStop("1.0", "red");
+
+        this.context.strokeStyle = gradient;
+
+        this.modelPlane.forEachInZLayer(0, (voxel, x, y) => {
+            
+            let voxelTransform = [
+                (x + 1) * this.view.zoom + this.context.lineWidth / 2,
+                (y + 1) * this.view.zoom + this.context.lineWidth / 2,
+                this.view.zoom - this.context.lineWidth,
+                this.view.zoom - this.context.lineWidth,
+            ];
+
+            let buffergrejimoj = Buffer.alloc(4);
+            buffergrejimoj.writeUInt32BE(voxel.value.color);
+            this.context.fillStyle = `#${buffergrejimoj.toString("hex")}`;
+            
+            let _strokeStyle = this.context.strokeStyle;
+            this.context.strokeStyle = this.context.fillStyle;
+
+            this.context.strokeRect(...voxelTransform);
+            this.context.fillRect(...voxelTransform);
+
+            this.context.strokeStyle = _strokeStyle;
+
+            if (voxel.value.selected)
+                this.context.strokeRect(...voxelTransform);
+        });
 
         this.context.lineWidth = 1;
         this.context.strokeStyle = "rgba(80, 80, 80, 1)";
 
         for (let x = 0; x < gridWidth; x++) {
             for (let y = 0; y < gridHeight; y++) {
-                this.context.strokeRect(x * this.view.zoom, y * this.view.zoom, this.view.zoom, this.view.zoom);
+                this.context.strokeRect(
+                    x * this.view.zoom,
+                    y * this.view.zoom,
+                    this.view.zoom,
+                    this.view.zoom
+                );
             }
         }
     },
@@ -60,7 +120,10 @@ module.exports = (app) => ({
     zoom(amount) {
         this.setView({
             zoom: Math.max(
-                this.minZoom - 1 + (this.view.zoom - this.minZoom + 1) * this.zoomScrollFactor ** amount,
+                this.minZoom -
+                    1 +
+                    (this.view.zoom - this.minZoom + 1) *
+                        this.zoomScrollFactor ** amount,
                 this.minZoom
             ),
         });
