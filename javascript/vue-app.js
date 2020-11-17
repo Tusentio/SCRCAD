b92cfe1951fb0f2529baf739a8d432954a917e1f
Octaphous <pappkasse2121@gmail.com>
2020-11-17 13:20:38 +0100

    Add modelName variable

--- javascript/vue-app.js
+++ javascript/vue-app.js
@@ -6,6 +6,7 @@ module.exports = (app) => ({
         el: "#vue-wrapper",
         data: {
             app,
+            modelName: "Untitled",
             panels: {
                 editor: true,
                 preview: false,
