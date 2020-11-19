4307d905ae6f4cb0b10c7f7636630322a329ab4d
Keve1227 <killenlillen02@gmail.com>
2020-11-19 13:30:21 +0100

    Add save project function

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,54 @@
+const path = require("path");
+const fs = require("fs");
+const os = require("os");
+const Store = require("@tusent.io/json-database");
 const Model = require("./model.js");
 const VueApp = require("./vue-app.js");
+
+fs.promises.exists = async function (path) {
+    try {
+        await fs.promises.stat(path);
+        return true;
+    } catch {
+        return false;
+    }
+};
 
 const app = {
     model: null,
     vue: null,
+    tempStore: null,
     init(Vue) {
+        const scrcadTempPath = path.resolve(os.tmpdir(), "SCRCAD");
+        if (!fs.existsSync(scrcadTempPath)) {
+            fs.mkdirSync(scrcadTempPath, { recursive: true });
+        }
+
+        const tempStorePath = path.join(scrcadTempPath, "store.json");
+        this.tempStore = new Store(tempStorePath);
+
         this.model = new Model("Untitled");
         this.vue = new VueApp(Vue, this);
+    },
+    async saveProject() {
+        const projectsPath =
+            this.tempStore.get("projects_path") ||
+            this.tempStore.set("projects_path", path.resolve(os.homedir(), "SCRCAD", "Projects"));
+
+        if (!(await fs.promises.exists(projectsPath))) {
+            await fs.promises.mkdir(projectsPath, { recursive: true });
+        }
+
+        let fileName = this.model.name;
+        for (
+            let i = 1;
+            await fs.promises.exists(path.resolve(projectsPath, `${fileName}.scrcad`));
+            i++
+        ) {
+            fileName = `${this.model.name}-${i}`;
+        }
+
+        await this.model.save(path.resolve(projectsPath, `${fileName}.scrcad`));
     },
 };
 
